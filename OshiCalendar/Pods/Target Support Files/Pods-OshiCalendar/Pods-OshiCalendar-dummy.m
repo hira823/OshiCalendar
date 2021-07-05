b531903c5d8fec88d341cebeab67b7635e7af49f
hira <hirapoke823@gmail.com>
2021-07-05 20:33:44 +0900

    FSCalendarをインストール（コミット漏れ）

---
+++ OshiCalendar/Pods/Target Support Files/Pods-OshiCalendar/Pods-OshiCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OshiCalendar : NSObject
+@end
+@implementation PodsDummy_Pods_OshiCalendar
+@end
---
+++ OshiCalendar/Pods/Target Support Files/Pods-OshiCalendarTests/Pods-OshiCalendarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OshiCalendarTests : NSObject
+@end
+@implementation PodsDummy_Pods_OshiCalendarTests
+@end
